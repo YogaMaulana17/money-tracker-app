d05d448f42c84a42a977aca7e9d579d9682e8bf9
Yoga Maulana <yogamaulana1606@gmail.com>
2025-06-23 22:18:07 +0700

    add edit transactions

--- src/js/pages/transactions/edit.js
+++ src/js/pages/transactions/edit.js
@@ -1,7 +1,30 @@
 const Edit = {
   async init() {
+    this._initialUI();
     await this._initialData();
     this._initialListener();
+  },
+
+  _initialUI() {
+    const listInputRadioTransactionType = [
+      {
+        inputId: 'recordType1',
+        value: 'income',
+        caption: 'Pemasukan',
+        required: true,
+      },
+      {
+        inputId: 'recordType2',
+        value: 'expense',
+        caption: 'Pengeluaran',
+        required: true,
+      },
+    ];
+    const inputRadioTransactionTypeEdit = document.querySelector('#inputRadioTransactionTypeEdit');
+    inputRadioTransactionTypeEdit.setAttribute(
+      'listRadio',
+      JSON.stringify(listInputRadioTransactionType),
+    );
   },
 
   async _initialData() {
@@ -22,10 +45,10 @@ const Edit = {
   },
 
   _initialListener() {
-    const evidenceInput = document.querySelector('#validationCustomEvidence');
-    evidenceInput.addEventListener('change', () => {
-      this._updatePhotoPreview();
-    });
+    // const evidenceInput = document.querySelector('#validationCustomEvidence');
+    // evidenceInput.addEventListener('change', () => {
+    //   this._updatePhotoPreview();
+    // });
 
     const editRecordForm = document.querySelector('#editRecordForm');
     editRecordForm.addEventListener(
@@ -70,24 +93,24 @@ const Edit = {
     };
   },
 
-  _updatePhotoPreview() {
-    const evidenceImg = document.querySelector('#validationCustomEvidenceImg');
-    const evidenceImgChange = document.querySelector('#validationCustomEvidenceImgChange');
-    const evidenceImgInput = document.querySelector('#validationCustomEvidence');
+  // _updatePhotoPreview() {
+  //   const evidenceImg = document.querySelector('#validationCustomEvidenceImg');
+  //   const evidenceImgChange = document.querySelector('#validationCustomEvidenceImgChange');
+  //   const evidenceImgInput = document.querySelector('#validationCustomEvidence');
 
-    const photo = evidenceImgInput.files[0];
-    if (!photo) return;
+  //   const photo = evidenceImgInput.files[0];
+  //   if (!photo) return;
 
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      evidenceImg.classList.add('d-none');
-      evidenceImgChange.classList.remove('d-none');
+  //   const reader = new FileReader();
+  //   reader.onload = (event) => {
+  //     evidenceImg.classList.add('d-none');
+  //     evidenceImgChange.classList.remove('d-none');
 
-      evidenceImgChange.style.backgroundImage = `url('${event.target.result}')`;
-    };
+  //     evidenceImgChange.style.backgroundImage = `url('${event.target.result}')`;
+  //   };
 
-    reader.readAsDataURL(photo);
-  },
+  //   reader.readAsDataURL(photo);
+  // },
 
   _populateTransactionToForm(transactionRecord = null) {
     if (!(typeof transactionRecord === 'object')) {
@@ -99,19 +122,33 @@ const Edit = {
     const nameInput = document.querySelector('#validationCustomRecordName');
     const amountInput = document.querySelector('#validationCustomAmount');
     const dateInput = document.querySelector('#validationCustomDate');
-    const evidenceInput = document.querySelector('#validationCustomEvidenceImg');
+    // const evidenceInput = document.querySelector('#validationCustomEvidenceImg');
+    const inputImagePreviewEdit = document.querySelector('#inputImagePreviewEdit');
     const descriptionInput = document.querySelector('#validationCustomNotes');
-    const typesInput = document.querySelectorAll('input[name="recordType"]');
+    // const typesInput = document.querySelectorAll('input[name="recordType"]');
+    const inputRadioTransactionTypeEdit = document.querySelector('#inputRadioTransactionTypeEdit');
 
     nameInput.value = transactionRecord.name;
     amountInput.value = transactionRecord.amount;
     dateInput.value = transactionRecord.date;
-    evidenceInput.setAttribute('src', transactionRecord.evidenceUrl);
-    evidenceInput.setAttribute('alt', transactionRecord.name);
+    // evidenceInput.setAttribute('src', transactionRecord.evidenceUrl);
+    // evidenceInput.setAttribute('alt', transactionRecord.name);
+    inputImagePreviewEdit.setAttribute('defaultImage', transactionRecord.evidenceUrl);
+    inputImagePreviewEdit.setAttribute('defaultImageAlt', transactionRecord.name);
     descriptionInput.value = transactionRecord.description;
-    typesInput.forEach((item) => {
+    // typesInput.forEach((item) => {
+    //   item.checked = item.value === transactionRecord.type;
+    // });
+    const listInputRadioTransactionType = JSON.parse(
+      inputRadioTransactionTypeEdit.getAttribute('listRadio'),
+    );
+    listInputRadioTransactionType.forEach((item) => {
       item.checked = item.value === transactionRecord.type;
     });
+    inputRadioTransactionTypeEdit.setAttribute(
+      'listRadio',
+      JSON.stringify(listInputRadioTransactionType),
+    );
   },
 
   _validateFormData(formData) {
